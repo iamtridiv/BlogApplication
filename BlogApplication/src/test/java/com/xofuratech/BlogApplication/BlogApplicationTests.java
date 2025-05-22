4a6529c17cfdf14b348458a30e29c94f5a152da7
iamtridiv <tridivkumar@gmail.com>
2025-05-22 11:39:11 +0530

    Init

---
+++ BlogApplication/src/test/java/com/xofuratech/BlogApplication/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xofuratech.BlogApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
