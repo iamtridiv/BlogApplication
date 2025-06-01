3a7c9ae35ff42451e8391203c4c1b7c1b87dfeda
iamtridiv <tridivkumar@gmail.com>
2025-06-01 10:38:32 +0530

    User controller create

---
+++ BlogApplication/src/main/java/com/xofuratech/BlogApplication/controllers/UserController.java
@@ -0,0 +1,55 @@
+package com.xofuratech.BlogApplication.controllers;
+
+import com.xofuratech.BlogApplication.entities.User;
+import com.xofuratech.BlogApplication.payloads.UserDto;
+import com.xofuratech.BlogApplication.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    // POST: Create User
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto createdUser = userService.createUser(userDto);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+    // PUT: Update User
+    @PutMapping
+    public ResponseEntity<UserDto> updateUser(@Validated @RequestBody UserDto userDto, @PathVariable int userId) {
+        UserDto updatedUser = userService.updateUser(userDto, userId);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    //Get a User by ID
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable int userId) {
+        UserDto user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    // Get all Users
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+       List<UserDto> users = userService.getAllUsers();
+       return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    // Delete a User by ID
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> deteteUser(@PathVariable int userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+    }
+
+
+
+}
